a2b1a4a8b3bd28000233987a3a8ecce1f285f108
readerx <gnolld@foxmail.com>
2020-03-09 16:05:26 +0800

    区分不同的地址类型

--- src/registryui/main.go
+++ src/registryui/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"log"
-	"net/url"
 	"registryui/client"
 	"registryui/view"
 
@@ -13,35 +12,32 @@ import (
 var (
 	basepath = "/registry"
 	debug    = true
-	registry = "https://192.168.1.254:5000"
+	registry = "192.168.1.254:5000"
+	access   = "https://registry:5000"
 	listen   = "0.0.0.0:8080"
 )
 
 func init() {
 	flag.StringVar(&basepath, "basepath", basepath, "Base path of Docker Registry UI")
 	flag.BoolVar(&debug, "debug", debug, "Debug mode. Affects only templates")
-	flag.StringVar(&registry, "registry", registry, "Registry URL with schema and port")
+	flag.StringVar(&registry, "registry", registry, "Registry URL with schema and port for view")
+	flag.StringVar(&access, "access", access, "Registry access address")
 	flag.StringVar(&listen, "listen", listen, "Listen address")
 }
 
 func main() {
 	flag.Parse()
 
-	u, err := url.Parse(registry)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// 初始化数据层，用户获取registry数据
-	registry, err := client.NewRegistry(registry)
+	r, err := client.NewRegistry(access)
 	if err != nil {
 		log.Fatal(err)
 	}
-	model := view.NewModel(basepath, registry)
+	model := view.NewModel(basepath, r)
 
 	// 初始化模板引擎
 	e := echo.New()
-	e.Renderer = view.SetupRender(debug, u.Host, basepath)
+	e.Renderer = view.SetupRender(debug, registry, basepath)
 
 	e.Static(basepath+"/static", "resources/static")
 	e.File("/favicon.ico", "resources/static/favicon.ico")
